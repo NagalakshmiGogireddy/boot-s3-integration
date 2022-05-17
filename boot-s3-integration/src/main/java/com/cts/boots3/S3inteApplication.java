c79c5956d07c5d5fab860e5a32371ba49d8275e1
Nagalakshmigogireddy <nagalakshmi.gogireddy@jobiak.com>
2022-05-17 12:42:12 +0530

    finish

---
+++ boot-s3-integration/src/main/java/com/cts/boots3/S3inteApplication.java
@@ -0,0 +1,13 @@
+package com.cts.boots3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3inteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3inteApplication.class, args);
+	}
+
+}
